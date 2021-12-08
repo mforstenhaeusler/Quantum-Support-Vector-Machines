a23a2b8b52bec6bdbbef2e8beda85c7a241c4c01
Maximilian Forstenhaeusler <Maxi@d3dbb9ba-f6ca-467f-a6b7-cfe47b85c4dc.fritz.box>
2021-12-08 17:23:17 +0100

    add ZFeatureMap

--- quantum_svm/quantum/feature_maps.py
+++ quantum_svm/quantum/feature_maps.py
@@ -4,7 +4,7 @@ from qiskit import QuantumCircuit
 class ZZFeatureMap:
     def __init__(self, n_qubits, reps, data_map, insert_barriers=False) -> None:
         """ ZZFeature Map
-        Implements Equation ... from slides.
+        Special Pauli-Feature Map. Implements Equation 41 from slides.
 
         Params:
         -------
@@ -13,8 +13,8 @@ class ZZFeatureMap:
         reps : int
                number of repetitions of unitary operator 
         
-        data_map : data map class
-                   data map class that is used to encode the classical data
+        data_map : float
+                   Data map function, f: R^n -> R
 
         insert_barriers : bool
                           if true, inserts barriers into the quantum circuit in qiskit
@@ -54,3 +54,52 @@ class ZZFeatureMap:
     
     def __repr__(self) -> str:
         return f"ZZFeatureMap(feature_dimensions={self.n_qubits}, reps={self.reps})"
+
+
+class ZFeatureMap:
+    def __init__(self, n_qubits, reps, data_map, insert_barriers=False) -> None:
+        """ ZFeature Map
+        Special Pauli-Feature Map
+
+        Params:
+        -------
+        n_qubits : int 
+                   number of qubits
+        reps : int
+               number of repetitions of unitary operator 
+        
+        data_map : float
+                   Data map function, f: R^n -> R
+
+        insert_barriers : bool
+                          if true, inserts barriers into the quantum circuit in qiskit
+        """
+        self.n_qubits = n_qubits
+        self.data_map = data_map
+        self.reps = reps
+        self.insert_barriers = insert_barriers
+        self._circuit = None
+    
+    def map(self, data, reverse=False):
+        """ Function that builds the parameterized quantum circuit of the Z-Feature Map"""
+        circuit = QuantumCircuit(self.n_qubits)
+        for i in range(self.reps):
+            if i > 0:
+                if self.insert_barriers: circuit.barrier()
+            circuit.h(0)
+            circuit.h(1)
+            if self.insert_barriers: circuit.barrier()
+            if not reverse:  
+                circuit.u(0,0,2*self.data_map.map(data[:1]),0)
+                circuit.u(0,0,2*self.data_map.map(data[1:]),1)
+            else:
+                circuit.u(0,0,-2*self.data_map.map(data[:1]),0)
+                circuit.u(0,0,-2*self.data_map.map(data[1:]),1)     
+        
+        if not reverse: 
+            return circuit.to_instruction()
+        else:
+            return circuit.to_instruction().reverse_ops()
+    
+    def __repr__(self) -> str:
+        return f"ZFeatureMap(feature_dimensions={self.n_qubits}, reps={self.reps})"
